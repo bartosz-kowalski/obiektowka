e5df15b15689d326957f5b8fe9defb695376b769
Bartosz Kowalski <bartekleonkowalski@gmail.com>
2025-06-04 18:52:13 +0200

    podzial na pliki, poprawa wizualizacji

--- main.cpp
+++ main.cpp
@@ -2,15 +2,70 @@
 #include "czesci.hpp"
 #include "rozne.hpp"
 
+#define MAX_SEGMENTS 10000
+Vector3 path[MAX_SEGMENTS];
+int pathCount = 0;
+Mesh filamentMesh = { 0 };
+Model filamentModel = { 0 };
+bool filamentDirty = true;
+float filamentWidth = 0.05f;
+
+bool CheckCollisionRayTriangle(Ray ray, Vector3 p1, Vector3 p2, Vector3 p3, Vector3* out)
+{
+
+	Vector3 edge1 = Vector3Subtract(p2, p1);
+	Vector3 edge2 = Vector3Subtract(p3, p1);
+	Vector3 h = Vector3CrossProduct(ray.direction, edge2);
+	float a = Vector3DotProduct(edge1, h);
+	if (a > -EPSILON && a < EPSILON) return false;
+
+	float f = 1.0f / a;
+	Vector3 s = Vector3Subtract(ray.position, p1);
+	float u = f * Vector3DotProduct(s, h);
+	if (u < 0.0f || u > 1.0f) return false;
+
+	Vector3 q = Vector3CrossProduct(s, edge1);
+	float v = f * Vector3DotProduct(ray.direction, q);
+	if (v < 0.0f || u + v > 1.0f) return false;
+
+	float t = f * Vector3DotProduct(edge2, q);
+	if (t > EPSILON) {
+		if (out != NULL) {
+			*out = Vector3Add(ray.position, Vector3Scale(ray.direction, t));
+		}
+		return true;
+	}
+
+	return false;
+}
+
 int main() {
-	//std::cout << "Working directory: " << std::filesystem::current_path() << "\n";
+
+	InitWindow(1000, 800, "Wizualizacja nawijarki");
+	SetTargetFPS(60);
 	bool automat = false;
 	bool stop = false;
 	float rotationTR = 0.0;
 	float rotationMA = 0.0;
+	double kC = 0, kM = 0, kS = 0, kT = 0, kMA = 0, kTR = 0;
+	double yC, yM, yT, yS, yCm, yMm, yTm, ySm, yCM = 800, yMM = 400, yTM = 401, ySM = 400;
+	bool working = false;
+	int iterator = 0, rotationSH = 0;
+	float wheel;
+	int gearMod = 2;
+	std::ifstream gcode;
+	std::string linia;
 
-	InitWindow(1000, 800, "Wizualizacja nawijarki");
-	SetTargetFPS(60);
+	Shader shader = LoadShader("lighting.vs", "lighting.fs");
+	int lightPosLoc = GetShaderLocation(shader, "lightPos");
+	int viewPosLoc = GetShaderLocation(shader, "viewPos");
+	int lightColorLoc = GetShaderLocation(shader, "lightColor");
+	int objectColorLoc = GetShaderLocation(shader, "objectColor");
+	Vector3 lightPos = { 0.0f, 4.0f, 0.0f };
+	Vector4 lightColor = { 1.0f, 1.0f, 1.0f, 1.0f };
+	Vector4 objectColor = { 0.5f, 0.5f, 0.5f, 1.0f };
+
+	FilePathList droppedFiles;
 
 	Guzik Xplus{ "Menu/Xplus.png", {64, 32} };
 	Guzik Xmin{ "Menu/Xmin.png", {138, 32} };
@@ -22,12 +77,12 @@ int main() {
 	Guzik DWUA{ "Menu/DwuA.png", {730, 32} };
 	Guzik Estop{ "Menu/Estop.png", {804, 32} };
 
-	Camera3D camera = { 0 };
-	camera.position = { 1.0f, 10.0f, 0.0f };  // Camera position
-	camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
-	camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-	camera.fovy = 60.0f;
 
+	Camera3D camera;;
+	camera.position = { 1.0f, 10.0f, 0.0f };
+	camera.target = { 0.0f, 0.0f, 0.0f };
+	camera.up = { 0.0f, 1.0f, 0.0f };
+	camera.fovy = 60.0f;
 	camera.projection = CAMERA_PERSPECTIVE;
 
 	std::vector <czesc> czesci;
@@ -72,54 +127,32 @@ int main() {
 		}
 	}
 
-	Shader shader = LoadShader("lighting.vs", "lighting.fs");
-
-	int lightPosLoc = GetShaderLocation(shader, "lightPos");
-	int viewPosLoc = GetShaderLocation(shader, "viewPos");
-	int lightColorLoc = GetShaderLocation(shader, "lightColor");
-	int objectColorLoc = GetShaderLocation(shader, "objectColor");
-
-	Vector3 lightPos = { 0.0f, 4.0f, 0.0f };
-	Vector4 lightColor = { 1.0f, 1.0f, 1.0f, 1.0f };
-	Vector4 objectColor = { 0.5f, 0.5f, 0.5f, 1.0f };
-
-	double yC, yM, yT, yS, yCm, yMm, yTm, ySm, yCM, yMM, yTM, ySM;
 	for (czesc& part : czesci)
 	{
 		part.getModel().materials[0].shader = shader;
 		if (part.getName() == "Carrage.obj") {
 			yC = yCm = part.getPosition().z;
-			//std::cout << part.getPosition().x << ' ' << part.getPosition().y << ' ' << part.getPosition().z << '\n';
 		}
 		if (part.getName() == "Motor Gear.obj") {
 			yM = yMm = part.getPosition().z;
-			//std::cout << part.getPosition().x << ' ' << part.getPosition().y << ' ' << part.getPosition().z << '\n';
 		}
 
 		if (part.getName() == "Tool roller.obj") {
 			yT = yTm = part.getPosition().z;
-			//std::cout << part.getPosition().x << ' ' << part.getPosition().y << ' ' << part.getPosition().z << '\n';
 		}
 
 		if (part.getName() == "Spool hookers.obj") {
 			yS = ySm = part.getPosition().z;
-			//std::cout << part.getPosition().x << ' ' << part.getPosition().y << ' ' << part.getPosition().z << '\n';
 		}
 
 	}
-	double gC, gM, gS, gT, gMA, gTR;
-	double kC = 0, kM = 0, kS = 0, kT = 0, kMA = 0, kTR = 0;
 
-	yCM = 800;
-	yMM = ySM = 400;
-	yTM = 401;
-	bool working = false;
-	std::ifstream gcode;
-	std::string linia;
-	FilePathList droppedFiles;
-	int iterator = 0;
+	Ray filament_r;
+	filament_r.direction = { 1,0,0 };
+	filament_r.position = { 0,0,0 };
 
 	while (!WindowShouldClose()) {
+		rotationSH %= 360;
 
 		SetShaderValue(shader, lightPosLoc, &lightPos, SHADER_UNIFORM_VEC3);
 		SetShaderValue(shader, viewPosLoc, &camera.position, SHADER_UNIFORM_VEC3);
@@ -129,7 +162,7 @@ int main() {
 		Vector2 mousePosition = GetMousePosition();
 		bool mousePressed = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
 		bool mouseReleased = IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
-		if ((DWUR.Wcisniety(mousePosition, mouseReleased) or DWUA.Wcisniety(mousePosition, mouseReleased)) && !working)
+		if (((DWUR.Wcisniety(mousePosition, mouseReleased) or DWUA.Wcisniety(mousePosition, mouseReleased)) || IsKeyPressed(KEY_R)) && !working)
 		{
 			automat = !automat;
 		}
@@ -141,7 +174,7 @@ int main() {
 				yT += 1;
 				yM += 1;
 				yS += 1;
-				//std::cout << yC << '\n';
+				rotationSH += 2;
 			}
 			if ((IsKeyDown(KEY_RIGHT) || Xmin.Wcisniety(mousePosition, mousePressed)) && yC > yCm)
 			{
@@ -149,28 +182,29 @@ int main() {
 				yT -= 1;
 				yM -= 1;
 				yS -= 1;
+				rotationSH += 2;
 			}
 			if (IsKeyDown(KEY_W) || TRplus.Wcisniety(mousePosition, mousePressed))
 			{
 				rotationTR += 2.0f;
-				//std::cout << "Obrót TR+: " << rotationTR << " stopni\n";
+				rotationSH += 1;
 			}
 
 			if (IsKeyDown(KEY_S) || TRmin.Wcisniety(mousePosition, mousePressed))
 			{
 				rotationTR -= 2.0f;
-				//std::cout << "Obrót TR-: " << rotationTR << " stopni\n";
+				rotationSH += 1;
 			}
 			if (IsKeyDown(KEY_A) || MAplus.Wcisniety(mousePosition, mousePressed))
 			{
 				rotationMA += 2.0f;
-				//std::cout << "Obrót MA+: " << rotationTR << " stopni\n";
+				rotationSH += 2;
 			}
 
 			if (IsKeyDown(KEY_D) || MAmin.Wcisniety(mousePosition, mousePressed))
 			{
 				rotationMA -= 2.0f;
-				//std::cout << "Obrót MA-: " << rotationTR << " stopni\n";
+				rotationSH += 2;
 			}
 		}
 		else if (!working) {
@@ -183,9 +217,11 @@ int main() {
 					std::filesystem::path plik = std::string(droppedFiles.paths[0]);
 
 					if (!std::filesystem::exists(plik) || plik.extension() != ".gcode") {
-						//kill yourself??????????
+						std::cerr << "Nie ma takiego pliku albo jest to plik nieodpowiedniego typu \n";
 					}
 					else {
+						rotationMA = static_cast<int>(rotationMA) % 360;
+						rotationTR = static_cast<int>(rotationTR) % 360;
 						iterator = 0;
 						gcode.open(plik);
 						working = true;
@@ -204,25 +240,18 @@ int main() {
 				char separator = ' ';
 				std::string token;
 				while (ss >> token) {
-					
-					if (token[0] == 'X') {
-						gC = normalize(yCm, std::stod(token.substr(1)), yCM);
-						gM = normalize(yMm, std::stod(token.substr(1)), yMM);
-						gT = normalize(yTm, std::stod(token.substr(1)), yTM);
-						gS = normalize(ySm, std::stod(token.substr(1)), ySM);
 
-						kC = (gC - yC) / 10;
-						kM = (gM - yM) / 10;
-						kS = (gS - yS) / 10;
-						kT = (gT - yT) / 10;
+					if (token[0] == 'X') {
+						kC = (normalize(yCm, std::stod(token.substr(1)), yCM) - yC) / 10;
+						kM = (normalize(yMm, std::stod(token.substr(1)), yMM) - yM) / 10;
+						kT = (normalize(yTm, std::stod(token.substr(1)), yTM) - yT) / 10;
+						kS = (normalize(ySm, std::stod(token.substr(1)), ySM) - yS) / 10;
 					}
 					else if (token[0] == 'Y') {
-						gMA = std::stod(token.substr(1));
-						kMA = (gMA - rotationMA) / 10;
+						kMA = (std::stod(token.substr(1)) - rotationMA) / 10;
 					}
 					else if (token[0] == 'Z') {
-						gTR = std::stod(token.substr(1));
-						kTR = (gTR - rotationTR) / 10;
+						kTR = (std::stod(token.substr(1)) - rotationTR) / 10;
 					}
 				}
 			}
@@ -230,12 +259,14 @@ int main() {
 
 		if (working)
 		{
-			if ((Estop.Wcisniety(mousePosition, mousePressed)))
+			if ((Estop.Wcisniety(mousePosition, mousePressed)) || IsKeyPressed(KEY_E))
 			{
 				stop = true;
 			}
 			if (stop)
 			{
+				rotationMA = static_cast<int>(rotationMA) % 360;
+				rotationTR = static_cast<int>(rotationTR) % 360;
 				gcode.close();
 				UnloadDroppedFiles(droppedFiles);
 				automat = !automat;
@@ -250,10 +281,10 @@ int main() {
 			yT += kT;
 			rotationMA += kMA;
 			rotationTR += kTR;
+			rotationSH += 2;
 			iterator += 1;
 		}
 
-		//Ruch kamery bo mnie już wkurzało że latała cały czas
 		if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
 			Vector2 delta = GetMouseDelta();
 			camera.target = Vector3Zero();
@@ -264,8 +295,7 @@ int main() {
 
 			UpdateCamera(&camera, CAMERA_FIRST_PERSON);
 		}
-		//przybliżanie środkowym myszy
-		float wheel = GetMouseWheelMove();
+		wheel = GetMouseWheelMove();
 		if (wheel != 0) {
 			Vector3 forward = Vector3Normalize(Vector3Subtract(camera.target, camera.position));
 			camera.position = Vector3Add(camera.position, Vector3Scale(forward, wheel * 0.5f));
@@ -274,11 +304,8 @@ int main() {
 		ClearBackground(RAYWHITE);
 
 		BeginMode3D(camera);
-
-
-		float rotation = 0;
-		int gearMod = 2;
-
+		Mesh mesh;
+		Matrix transform;
 		for (czesc& part : czesci) {
 			Vector3 pos = part.getPosition();
 			if (!part.isMovable())
@@ -296,6 +323,8 @@ int main() {
 					pos.z = yT;
 					part.setPosition(pos);
 					part.Draw(pos, { rotationTR, 0, 0 });
+					filament_r.position = pos;
+					filament_r.direction = { 1, 0, 0 };
 				}
 				else if (part.getName() == "Carrage.obj") {
 					pos.z = yC;
@@ -305,14 +334,45 @@ int main() {
 				else if (part.getName() == "Spool hookers.obj") {
 					pos.z = yS;
 					part.setPosition(pos);
-					part.Draw(pos, { 0, 0, rotationMA * 0.9270072f });
+					part.Draw(pos, { 0, 0, static_cast<float>(rotationSH) });
 				}
 				else if (part.getName() == "Mandrel.obj") {
 					part.setPosition(pos);
 					part.Draw(pos, { 0, 0, rotationMA });
+					mesh = part.getModel().meshes[0];
+					transform = part.getModel().transform;
 				}
 			}
 		}
+
+		/*for (int i = 0; i < mesh.triangleCount; i++) {
+			int i0 = mesh.indices[i * 3 + 0];
+			int i1 = mesh.indices[i * 3 + 1];
+			int i2 = mesh.indices[i * 3 + 2];
+
+			Vector3 v0 = Vector3Transform(
+				{
+				mesh.vertices[i0 * 3 + 0], mesh.vertices[i0 * 3 + 1], mesh.vertices[i0 * 3 + 2]
+				},
+				transform);
+			Vector3 v1 = Vector3Transform(
+				{
+				mesh.vertices[i1 * 3 + 0], mesh.vertices[i1 * 3 + 1], mesh.vertices[i1 * 3 + 2]
+				},
+				transform);
+			Vector3 v2 = Vector3Transform(
+				{
+				mesh.vertices[i2 * 3 + 0], mesh.vertices[i2 * 3 + 1], mesh.vertices[i2 * 3 + 2]
+				},
+				transform);
+
+			Vector3 hitPos = { 0 };
+			if (CheckCollisionRayTriangle(filament_r, v0, v1, v2, &hitPos)) {
+				std::cout << hitPos.x << ' ' << hitPos.y << ' ' << hitPos.z << std::endl;
+				break;
+			}
+		}*/
+		//break;
 
 		DrawGrid(10, 1.0f);
 		EndMode3D();
